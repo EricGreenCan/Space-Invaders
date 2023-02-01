0ccd3b6592f15b29def1af4cbc6c3af29235a717
EricGreenCan <114961732+EricGreenCan@users.noreply.github.com>
2023-02-01 10:33:42 -0500

    Add files via upload

---
+++ SpaceInvaders.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class SpaceInvaders extends Application{
+	
+	//variables
+	private static final int PLAYER_SIZE = 50;
+	private static final int HEIGHT = 800;
+	private static final int WIDTH = 1000;
+	private static final Random RAND = new Random();
+	
+	//images
+	static final Image PLAYER = new Image
+			("C:\\Users\\green\\eclipse-workspace\\spaceInvaders\\Images\\player.png");
+	
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
