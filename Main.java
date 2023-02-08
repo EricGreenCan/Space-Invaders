b3f76918388b15134b37d14a8731d194935cdd7c
EricGreenCan <114961732+EricGreenCan@users.noreply.github.com>
2023-02-08 10:12:20 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,111 @@
 package application;
 	
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 
 public class Main extends Application {
+	
+	private static final Pane root = new Pane();
+	private static int t = 0;
+	private static Sprite player = new Sprite
+			(50, 50, 375, 400, Color.BLACK, "player");
+	
+	private static Parent createContent(){
+		root.getChildren().add(player);
+		createEnemies();
+		AnimationTimer updater = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				update();
+			}
+			
+		};
+		updater.start();
+		return root;
+	}
+	
+//	private static List<Sprite> sprites(){
+//		return root.getChildren().stream().map(node -> (Sprite)node).collect(Collectors.toList());
+//	}
+	
+	private static void update() {
+		t+=0.016;
+	}
+
+	private static void createEnemies() {
+		for (int i = 0; i < 6; i++) {
+			root.getChildren().add(new Sprite
+					(50, 50,75 + i*150, 100, Color.ORANGE, "enemy"));
+		}
+		
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+	public void start(Stage primaryStage){
+		Scene scene = new Scene(createContent(), 800, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		scene.setOnKeyPressed(event -> {
+			switch (event.getCode()) {
+				case LEFT:
+					player.moveLeft();
+					break;
+				case RIGHT:
+					player.moveRight();
+					break;
+				case SPACE:
+					player.shoot();
+					break;
+			}
+		});
+	}
+	
+	private static class Sprite extends Rectangle{
+		
+		private double x;
+		private double y;
+		private String type;
+		
+		public Sprite(double w, double h, double x, double y, Color clr, String type) {
+			super(w, h, clr);
+			this.x = x;
+			this.y = y;
+			this.type = type;
+			
+			setLayoutX(x);
+			setLayoutY(y);
+		}
+		
+		public void moveRight() {
+			setLayoutX(getLayoutX()+20);
+		}
+		
+		public void moveLeft() {
+			setLayoutX(getLayoutX()-20);
+		}
+		
+		public void moveUp() {
+			setLayoutY(getLayoutY()-5);
+		}
+		
+		public void moveDown() {			
+			setLayoutY(getLayoutY()+5);
+		}
+		
+		public void shoot() {
+			root.getChildren().add(new Sprite
+					(5, 20, getLayoutX()+25, getLayoutY(), Color.GREENYELLOW, type + "bullet"));
 		}
 	}
 	
---
+++ module-info.java
@@ -0,0 +1,6 @@
+module spaceInvaders {
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
